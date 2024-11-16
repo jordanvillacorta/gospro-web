3e46e43b9a8d01f65c4aea98ebc7d3fe6f78db9f
Jordan Villacorta <jordanvillacorta@jordans-air.attlocal.net>
2024-11-16 11:57:11 -0600

    Add all config files and vite, tts, and postcss files to complete jsx to tsx. Add module css files for NavBar and SearchBar.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import ShopDetail from './components/ShopDetail';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/shop/:id" element={<ShopDetail />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+  return <Home />;
+};
 
 export default App;
